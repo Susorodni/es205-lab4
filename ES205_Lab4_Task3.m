5a48bc18c480d3aa2ace50e2fabbe06fc351ff31
Luke Gaston <lgaston@myhoosierneighbors.com>
2025-04-04 10:03:59 -0400

    Changed task 3 to match main

--- ES205_Lab4_Task3.m
+++ ES205_Lab4_Task3.m
@@ -1,40 +1,46 @@
 % ES205 Lab 4 Task 3
 clear variables; close all; clc
 
-E_a = 68.9; % GPa
-rho_a = 2700; %kg/m^3
+%% Beam Properties
+rho_Alum = 2700;                                        % kg/m^3
+rho_Steel = 7870;                                       % kg/m^3
+E_Alum = 68.9*(10^9);                                   % Pa
+E_Steel = 186*(10^9);                                   % Pa
 
-% convert to pascals
-E_a = E_a*1e9; % Pa
+% Dimensions
+h = 0.123/39.37;                                        % m
+b = 1.000/39.37;                                        % m
+L = 12.000/39.37;                                       % m
 
-h = 0.123; % in
-b = 1.000; % in
-L = 12; % in
+%% Masses of Beams
+V = L*b*h;                                              % m^3
+m_Alum_eq = rho_Alum*V*0.23;                            % kg
+m_Steel_eq = rho_Steel*V*0.23;                          % kg
 
-% convert to meters
-h = h/39.37; % m
-b = b/39.37; % m
-L = L/39.37; % m
+%% Stiffness of Beams
+I = (1/12)*b*(h^3);                                     % m^4
+k_Alum = (3*E_Alum*I)/(L^3);                            % ???
+k_Steel = (3*E_Steel*I)/(L^3);
 
+%% Predicted Natural Frequency
+omegan_Alum_init = sqrt(k_Alum/m_Alum_eq);              % rad/s
+omegan_Steel_init = sqrt(k_Steel/m_Steel_eq);           % rad/s
+fn_Alum_init = omegan_Alum_init/(2*pi);                 % Hz
+fn_Steel_init = omegan_Steel_init/(2*pi);               % Hz
 
-m_eq_a = 0.23*rho_a*b*h*L;
-I_a = (b*h^3)/12;
-k_a = 3*E_a*I_a/(L^3);
+%% Mass Adjustment
+% Adjust until fn_Alum_final becomes 24.98 Hz
+m_adj_Alum = 0.0041;                                      % kg
 
-wn_a = sqrt(k_a/m_eq_a); % rad/s
-fn_a_i = wn_a/(2*pi); % Hz
+m_Alum_final= m_Alum_eq + m_adj_Alum;                   % kg
 
+%% Natural Frequency Adjustment
+omegan_Alum_final = sqrt(k_Alum/m_Alum_final);
+fn_Alum_final = omegan_Alum_final/(2*pi);
 
-fn_a_act = 156.98811/(2*pi); % Hz
-
-fn_a = fn_a_i;
-tol = 1e-4;
-m_adj = 0;
-
-
-while abs(fn_a - fn_a_act) > tol
-    m_adj = m_adj + 0.0001;
-    fn_a = sqrt(k_a/(m_eq_a + m_adj*1e-3))/(2*pi);
-end
-
-m_adj
+%% Display Results
+disp('Table 2')
+disp('Aluminum')
+disp(['Initial Natural Frequency ', num2str(fn_Alum_init), ' Hz'])
+disp(['Mass Adjustment           '])
+disp(['Stiffness                 ', num2str(k_Alum), ' units'])
