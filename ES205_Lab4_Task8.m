aca5a3937dc62f0305c38d712b7bb7beaa5350cc
Tyson Patterson <tpcoltsfan2312@gmail.com>
2025-04-09 17:51:47 -0400

    Task 8

--- ES205_Lab4_Task8.m
+++ ES205_Lab4_Task8.m
@@ -1,13 +1,28 @@
-% Task 8
-clc; clear variables; close all;
+%% Task 8
+clear variables; close all; clc
 
-%% solve for transfer function
-syms R C m k c theta
-syms s F Y V
-eq1 = R*C*V*s + V + R*theta*Y*s == 0;
-eq2 = m*Y*s^2 + c*Y*s + k*Y - theta*V == F;
-equations = [eq1; eq2];
-unknowns = [V; Y];
-solutions = solve(equations, unknowns);
+ syms R C m k c theta
+ syms s F Y V
+
+eq1 = R*C*V*s +V + R*theta*Y*s == 0;
+eq2 = m*Y*s^2 +c*Y*s +k*Y - theta*V == F;
+
+equations = [eq1;eq2];
+unknowns = [V;Y];
+solutions = solve(equations,unknowns);
 G = solutions.V/F;
+
+
 pretty(G)
+
+
+X = readmatrix('task7_Parameters.csv');
+C = X(4);
+R = X(5);
+k = X(3);
+m = X(1);
+c = X(2);
+theta =X(6);
+
+poles = roots([(C*R*m)/k (C*R*c+m)/k (R*theta^2+c+C*R*k)/k]);
+zeros = roots([(R*theta)/k 0]);
